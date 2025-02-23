6cbc03f3de05c25d53acef8788524c2f95ab1125
Gopal Krishna Jha <141767531+gopal-krishna-jha@users.noreply.github.com>
2025-02-23 23:37:27 +0530

    [Gopal Krishna Jha] Add welcom file
    [Gopal Krishna Jha] Add welcome file

---
+++ WelcomeToSpringFramework.java
@@ -0,0 +1,7 @@
+//Create a WelcomeToSpringFramework to print the welcome statement
+public class WelcomeToSpringFramework {
+    public static void main(String[] args) {
+        System.out.println("\"Welcome to the Spring Framework By Gopal\"");
+    }
+
+}
